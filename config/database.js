fab509f0487d3b876e4bce80e205bda7698fe08b
Haikal Hidayatullah <haikal.hidayat.2103@gmail.com>
2022-06-10 14:00:04 +0700

    p

---
+++ config/database.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const path = require("path");
+const DB_TEST_FILE_PATH = path.join(__dirname, "../db/test.sqlite");
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+    ssl: true,
+  },
+  test: {
+    storage: DB_TEST_FILE_PATH,
+    logging: false,
+    dialect: "sqlite",
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+    ssl: true,
+  },
+};
