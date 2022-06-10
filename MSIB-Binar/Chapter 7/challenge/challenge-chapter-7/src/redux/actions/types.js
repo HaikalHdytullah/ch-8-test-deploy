7d18fc3314b0a83d03b07a83ac5258fb5f4932e4
Haikal Hidayatullah <83757209+HaikalHdytullah@users.noreply.github.com>
2022-06-10 13:43:03 +0700

    Delete MSIB-Binar directory

--- MSIB-Binar/Chapter 7/challenge/challenge-chapter-7/src/redux/actions/types.js
+++
@@ -1,8 +0,0 @@
-// ACTIONS FOR POSTS
-export const GET_ALL_CARS = "GET_ALL_CARS";
-export const CARS_ERROR = "CARS_ERROR";
-
-// ACTIONS FOR AUTHENTICATION/AUTHORIZATION
-export const LOGIN = "LOGIN";
-export const LOGOUT = "LOGOUT";
-export const AUTH_ERROR = "AUTH_ERROR";
--- MSIB-Binar/Chapter 7/challenge/challenge-chapter-7/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import carReducer from "./carReducer";
-
-export default combineReducers({
-  car: carReducer,
-});
